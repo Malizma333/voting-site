19ab34e5ff14ad8e4b0eb7f6dce443e6efee6f40
Malizma333 <malizma333@gmail.com>
2024-09-26 23:15:26 -0500

    voting frontend and init backend

---
+++ backend/index.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+import dotenv
+
+print("Running")
+
+# playlist_url = 'https://youtube.com/playlist?list=' + '&si=' + '&key='
+# requests.get(playlist_url)
