59ef58348f90d918987353fb18e110c062ac477b
Malizma333 <malizma333@gmail.com>
2024-09-29 15:14:10 -0500

    init svelte project

--- backend/setup.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-
-python -m venv .venv
-source .venv\\Scripts\\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-cd backend
-uvicorn main:app --reload
-deactivate
