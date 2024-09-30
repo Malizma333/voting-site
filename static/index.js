7b098b9b71484532b1c2e6695877220f96e90b01
Malizma333 <malizma333@gmail.com>
2024-09-29 19:43:05 -0500

    form submission and fetch

--- static/index.js
+++ static/index.js
@@ -1,3 +1,18 @@
-window.onload = () => {
-  
+window.onload = async () => {
+  const req = await fetch("/api/youtube_data");
+  const youtubePlaylist = await req.json();
+  console.log(youtubePlaylist);
+
+  const votingForm = document.querySelector("form");
+  const radioGroup = votingForm.querySelector("sl-radio-group");
+  const submitButton = votingForm.querySelector("sl-button");
+
+  submitButton.disabled = false;
+
+  votingForm.onsubmit = (e) => {
+    e.preventDefault();
+    if (radioGroup.value == '') return;
+    console.log(radioGroup.value);
+    radioGroup.value = '';
+  }
 }
